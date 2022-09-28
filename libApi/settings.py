c31f57da792e83c0693792a79469ba5b9298781f
mustafamordaa1 <88613079+mustafamordaa1@users.noreply.github.com>
2022-09-28 19:19:35 +0300

    Update settings.py

--- libApi/settings.py
+++ libApi/settings.py
@@ -16,9 +16,8 @@ import os
 
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
