8815e710ad805d226ade3e1a6eba801d0a85b576
mustafamordaa1 <88613079+mustafamordaa1@users.noreply.github.com>
2022-09-29 14:12:30 +0300

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 from . import views
-#from .views import RegisterView
+from .views import MyTokenObtainPairView
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -12,7 +12,7 @@ urlpatterns = [
 	path('books/<int:id>', views.book_detail, name="datails"),
 	path('borrow/', views.borrow_book),
 	path('borrow/<int:id>', views.user_books),
-	path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.create_auth)#RegisterView.as_view())
 ]
