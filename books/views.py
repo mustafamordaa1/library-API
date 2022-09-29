2da4022f5b0603a366a9c7343e34f5c86b9305ae
mustafamordaa1 <88613079+mustafamordaa1@users.noreply.github.com>
2022-09-29 15:58:00 +0300

    Update views.py

--- books/views.py
+++ books/views.py
@@ -65,7 +65,7 @@ def borrow_book(request):
       serializer = BorrowSerializer(data=request.data)
       if serializer.is_valid():
          serializer.save()
-	 return Response(serializer.data, status=status.HTTP_201_CREATED)
+         return Response(serializer.data, status=status.HTTP_201_CREATED)
 	                         	 
 @api_view(['GET', 'PUT', 'DELETE'])
 def user_books(request, id, format=None):
