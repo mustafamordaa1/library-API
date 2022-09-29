4ea51676dd08867a641b1829f826ba4047d3ecff
mustafamordaa1 <88613079+mustafamordaa1@users.noreply.github.com>
2022-09-29 14:19:03 +0300

    Update serializers.py

--- books/serializers.py
+++ books/serializers.py
@@ -3,6 +3,7 @@ from .models import Book, borrow
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class RegisterSerializer(serializers.ModelSerializer):
