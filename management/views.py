b62771f71d4b36d1ef5bca1ec610683b8289ab82
mustafamordaa1 <88613079+mustafamordaa1@users.noreply.github.com>
2022-09-29 16:08:20 +0300

    Update views.py

--- management/views.py
+++ management/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .serializers import BookSerializer, BorrowSerializer
+from books.serializers import BookSerializer, BorrowSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
